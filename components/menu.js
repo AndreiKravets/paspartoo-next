66b4d03ea0701a7b7a4b2647e745cea0bc7f3e14
AndreiKravets <tetamoed1@gmail.com>
2022-06-17 13:42:21 +0300

    new page services

--- components/menu.js
+++ components/menu.js
@@ -1,6 +1,5 @@
 import React, {useState,useEffect} from 'react'
 import Link from "next/link"
-import { BsCaretLeft } from "react-icons/bs"
 
 export default function Menu (data) {
     console.log(data)
@@ -25,7 +24,7 @@ export default function Menu (data) {
                     <Link href="">Services</Link>
                     <ul className="services_submenu">
                         {
-                            (sort_category ? sort_category.map((category)=>{
+                            (category ? category.map((category)=>{
                             return(
                                 <>
                                   {console.log(category)}
@@ -34,44 +33,6 @@ export default function Menu (data) {
                             )
                           }) :'')
                         }
-
-                        {/*<li className={activeSubmenu == 1 ? 'active' : ''} onClick={() => {setActiveSubmenu(1)}}>UX/UI Design <BsCaretLeft />*/}
-                        {/*<ul>*/}
-                        {/*    <li> <Link href="/services">Web Development Overview</Link></li>*/}
-                        {/*    <li> <Link href="/services">eCommerce Design and Development Solutions</Link></li>*/}
-                        {/*    <li> <Link href="/services">Landing Pages Development</Link></li>*/}
-                        {/*</ul>*/}
-                        {/*</li>*/}
-                        {/*<li className={activeSubmenu == 2 ? 'active' : ''} onClick={() => {setActiveSubmenu(2)}}>Web Development <BsCaretLeft />*/}
-                        {/*    <ul>*/}
-                        {/*        <li> <Link href="/services">Web Development Overview</Link></li>*/}
-                        {/*        <li> <Link href="/services">eCommerce Design and Development Solutions</Link></li>*/}
-                        {/*    </ul>*/}
-                        {/*</li>*/}
-                        {/*<li className={activeSubmenu == 3 ? 'active' : ''} onClick={() => {setActiveSubmenu(3)}}>Magento <BsCaretLeft />*/}
-                        {/*    <ul>*/}
-                        {/*        <li> <Link href="/services">Web Development Overview</Link></li>*/}
-                        {/*        <li> <Link href="/services">eCommerce Design and Development Solutions</Link></li>*/}
-                        {/*        <li> <Link href="/services">Landing Pages Development</Link></li>*/}
-                        {/*    </ul></li>*/}
-                        {/*<li className={activeSubmenu == 4 ? 'active' : ''} onClick={() => {setActiveSubmenu(4)}}>Shopify <BsCaretLeft />*/}
-                        {/*    <ul>*/}
-                        {/*        <li> <Link href="/services">Web Development Overview</Link></li>*/}
-                        {/*        <li> <Link href="/services">eCommerce Design and Development Solutions</Link></li>*/}
-                        {/*        <li> <Link href="/services">Landing Pages Development</Link></li>*/}
-                        {/*    </ul></li>*/}
-                        {/*<li className={activeSubmenu == 5 ? 'active' : ''} onClick={() => {setActiveSubmenu(5)}}>WooComerce <BsCaretLeft />*/}
-                        {/*    <ul>*/}
-                        {/*        <li> <Link href="/services">Web Development Overview</Link></li>*/}
-                        {/*        <li> <Link href="/services">eCommerce Design and Development Solutions</Link></li>*/}
-                        {/*        <li> <Link href="/services">Landing Pages Development</Link></li>*/}
-                        {/*    </ul></li>*/}
-                        {/*<li className={activeSubmenu == 6 ? 'active' : ''} onClick={() => {setActiveSubmenu(6)}}>WordPress <BsCaretLeft />*/}
-                        {/*    <ul>*/}
-                        {/*        <li> <Link href="/services">Web Development Overview</Link></li>*/}
-                        {/*        <li> <Link href="/services">eCommerce Design and Development Solutions</Link></li>*/}
-                        {/*        <li> <Link href="/services">Landing Pages Development</Link></li>*/}
-                        {/*    </ul></li>*/}
                     </ul>
                 </li>
                 <li>
